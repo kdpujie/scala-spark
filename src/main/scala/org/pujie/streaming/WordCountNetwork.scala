0e094000c9cb929bcad338f17e82a96028a1b657
pujie <pujie@baiwutong.com>
2015-10-29 09:48:33 +0800

    first commit

---
+++ src/main/scala/org/pujie/streaming/WordCountNetwork.scala
@@ -0,0 +1,30 @@
+package org.pujie.streaming
+
+import org.apache.spark.SparkConf
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+ * 从socket中读取数据，计算单词数量。
+ * @author pujie
+ */
+object WordCountNetwork {
+  def main(args:Array[String]){
+    // 检查输入参数是否合规
+    if(args.length != 3){
+      println("Usage:  <hostname> <port> <seconds>\n" + "In local mode, <master> should be 'local[n]' with n > 1")
+      System.exit(1)
+    }
+    val hostname = args(0)
+    val port = args(1).toInt
+    val seconds = args(2).toInt
+    val conf = new SparkConf().setAppName("wordCount-Network")
+    val ssc = new StreamingContext(conf,Seconds(seconds))
+    val lines = ssc.socketTextStream(hostname, port, StorageLevel.MEMORY_ONLY_SER);
+    val words = lines.flatMap { x => x.split(" ") }
+    val wordsCounts = words.map (x => (x,1)).reduceByKey( (x,y)=>x+y )
+    wordsCounts.print();
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
