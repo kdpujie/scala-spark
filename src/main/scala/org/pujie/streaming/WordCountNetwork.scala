5ed778d86eae6b0334b3efeca280b363ad2e8fa7
pujie <pujie@baiwutong.com>
2016-01-25 18:46:49 +0800

    add  stateful wordCount network

--- src/main/scala/org/pujie/streaming/WordCountNetwork.scala
+++ src/main/scala/org/pujie/streaming/WordCountNetwork.scala
@@ -12,18 +12,18 @@ object WordCountNetwork {
   def main(args:Array[String]){
     // 检查输入参数是否合规
     if(args.length != 3){
-      println("Usage:  <hostname> <port> <seconds>\n" + "In local mode, <master> should be 'local[n]' with n > 1")
+      println("Usage:  <hostname> <port> <batch-size>\n" + "In local mode, <master> should be 'local[n]' with n > 1")
       System.exit(1)
     }
     val hostname = args(0)
     val port = args(1).toInt
-    val seconds = args(2).toInt
+    val batchSize = args(2).toInt
     val conf = new SparkConf().setAppName("wordCount-Network")
-    val ssc = new StreamingContext(conf,Seconds(seconds))
-    val lines = ssc.socketTextStream(hostname, port, StorageLevel.MEMORY_ONLY_SER);
+    val ssc = new StreamingContext(conf,Seconds(batchSize))
+    val lines = ssc.socketTextStream(hostname, port, StorageLevel.MEMORY_ONLY_SER)
     val words = lines.flatMap { x => x.split(" ") }
     val wordsCounts = words.map (x => (x,1)).reduceByKey( (x,y)=>x+y )
-    wordsCounts.print();
+    wordsCounts.print()
     ssc.start()
     ssc.awaitTermination()
   }
